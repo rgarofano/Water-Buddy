da5a57021d3385334d3e5ee90348988043bfb103
Ian <thecruik@gmail.com>
2022-12-03 15:55:23 -0800

    [icruiksh] Add button module

---
+++ devices/Button.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "../hwCommon/SystemTools.h"
+#include "../hwCommon/GPIO.h"
+
+#include "Button.h"
+
+#define BUTTON_VALUE_PRESSED 1
+#define USER_BUTTON_VALUE_PRESSED 0
+
+void Button_init(char *pin, int gpioNum)
+{
+    configPin(pin, "gpio");
+    GPIO_configPin(pin, gpioNum, GPIO_IN);
+
+}
+
+bool Button_isPressed(int gpioNum)
+{
+    if(gpioNum == USER_BUTTON_GPIO_NUM) {
+        return (GPIO_read(gpioNum) == USER_BUTTON_VALUE_PRESSED);
+    }
+    return (GPIO_read(gpioNum) == BUTTON_VALUE_PRESSED);
+}
